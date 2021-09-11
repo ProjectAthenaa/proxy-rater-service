6413f64164966e364b27816c3a39b984523567b2
Kiwi <agiannoulis@mavestro.co>
2021-09-12 01:15:59 +0300

    .

--- service/listener.go
+++ service/listener.go
@@ -3,6 +3,7 @@ package service
 import (
 	"github.com/ProjectAthenaa/sonic-core/sonic/core"
 	"github.com/ProjectAthenaa/sonic-core/sonic/database/ent/product"
+	"github.com/prometheus/common/log"
 	"strings"
 )
 
@@ -10,6 +11,7 @@ func (r *Rater) listen() {
 	pubSub := core.Base.GetRedis("cache").PSubscribe(r.ctx, "proxies:*")
 
 	for msg := range pubSub.Channel() {
+		log.Info(msg.Channel, msg.Payload)
 		site := product.Site(strings.Split(msg.Channel, ":")[1])
 		go r.Rate(msg.Payload, site)
 	}
